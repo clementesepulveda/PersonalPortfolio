bd0343fdd9fed3568b607e5f0431f9ee29677dea
clementesepulveda <clemente.sepulveda@uc.cl>
2024-03-23 12:11:45 -0300

    refactor/elements for website

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer flex justify-center items-center p-1 bg-red-500">
+            <div>hello</div>
+            {/* <img src={bpeHorizontal} alt="bluepearl" class="object-stretch" /> */}
+            {/* <div class="copyright p-8">{$_("copyright")}</div> */}
+        </footer>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="h-10 bg-green-500">
+            <a href="/">Home</a>
+        </header>
+    )
+}
