9723adc4c8e8e0b7b7a39f16c8f8401a7fb7e8d5
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-03 21:56:52 -0300

    fix/styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,9 +6,6 @@ function Home() {
   return (
     <div className="App">
       <u>
-        <ul><a href="/projects">Projects</a></ul>
-        <ul>About</ul>
-        <ul>Socials</ul>
         <ul><a href="/boids">Boids</a></ul>
         <ul><a href="/abstract">Fun stuffs</a></ul>
       </u>
