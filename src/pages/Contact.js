821c90a02a268aabbf63aab54d4bf4bb965fe52b
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-04 13:14:15 -0300

    feat/contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+function Home() {
+
+
+    return (
+      <div className="App">
+        clemente.sepulveda@ucl
+      </div>
+    );
+  }
+  
+  export default Home;
+  
