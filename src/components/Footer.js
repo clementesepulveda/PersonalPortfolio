f53a8cdca7f5523a0a763a694a8156a027361e4d
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-03 21:55:59 -0300

    feat/style and pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="h-16 footer flex justify-center items-center">
-            <p>Clemente Sepúlveda 2023</p>
+            <p>Clemente Sepúlveda 2024</p>
         </footer>
     )
 }
