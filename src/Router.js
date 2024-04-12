61d45375004fe1ff23dd06494214a2487a18d7bb
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-11 23:17:25 -0400

    feat/admin login page

--- src/Router.js
+++ src/Router.js
@@ -1,58 +1,36 @@
 import {
-    createBrowserRouter,
-    RouterProvider,
+    BrowserRouter, Routes, Route
 } from "react-router-dom"
 
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import NotFound from "./pages/notFound";
-import Admin from "./pages/Admin";
+// import Admin from "./pages/Admin";
 import Boids from "./pages/boids/Boids";
 import Abstract from "./pages/Abstract";
 import Contact from "./pages/Contact";
+import LogIn from "./pages/admin/LogIn";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function Router() {
-    // initialize a browser router
-    const router = createBrowserRouter([{
-        element: <Layout />,
-        errorElement: <NotFound />,
-        children: [
-            
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-            path: "/projects",
-            element: <Projects />,
-        },
-        {
-            path: "/contact",
-            element: <Contact />,
-        },
-        {
-            path: "/boids",
-            element: <Boids />,
-        },
-        {
-            path: "/abstract",
-            element: <Abstract />,
-        },
-        {
-            path: "/admin",
-            element: <Admin />,
-        },
-        {
-            path: "*",
-            element: <NotFound />,
-        },
-        ]
-    }
-    ])
 
     return (
-        <RouterProvider router={router} />
+        <BrowserRouter >
+            <AuthProvider>
+                <Routes>
+                    <Route Component={Layout}>
+                        <Route path='/' Component={Home}></Route>
+                        <Route path='/projects' Component={Projects}></Route>
+                        <Route path='/contact' Component={Contact}></Route>
+                        <Route path='/abstract' Component={Abstract}></Route>
+                        <Route path='/boids' Component={Boids}></Route>
+                        <Route path='/admin' Component={LogIn}></Route>
+                        <Route path='/*' Component={NotFound}></Route>
+                    </Route>
+                </Routes >
+            </AuthProvider>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/admin/LogIn.js
@@ -0,0 +1,55 @@
+
+import React, { useRef, useState } from "react"
+// import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../../contexts/AuthContext"
+import { useNavigate  } from "react-router-dom"
+
+export default function LogIn() {
+  const navigate = useNavigate();
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { login, currentUser } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      navigate("/")
+    } catch {
+      setError("Failed to log in")
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <div className="flex justify-center bg-gren-500">
+        <div className="dark:bg-slate-700 shadow-2xl border rounded-xl border-slate-200 dark:border-slate-500 p-6 m-6 w-screen max-w-xl">
+          
+          <h2 className="text-center mb-4 text-3xl">Log In</h2>
+
+          {error && <div className="bg-red-400 rounded p-2 mb-4 text-center">{error}</div>}
+
+          <form onSubmit={handleSubmit} className="flex flex-col">
+              <label>Email</label>
+              <input type="email" ref={emailRef} className="text-black mt-2 rounded border p-1" required />
+
+              <label className="mt-4">Password</label>
+              <input type="password" ref={passwordRef} className="text-black mt-2 rounded border p-1" required />
+
+            <button disabled={loading} className="bg-slate-800 text-white w-full rounded p-2 mt-8" type="submit">
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
