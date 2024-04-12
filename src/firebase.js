af457736d6ed65d39bc404094fbf8f7ad70c7793
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-11 23:31:30 -0400

    deleted debug print

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,6 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-console.log(process.env.REACT_APP_ENV_TEST)
-
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
