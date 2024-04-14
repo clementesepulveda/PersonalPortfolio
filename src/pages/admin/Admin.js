cc58d15252ac2dd8d40abb51049daaf5afc6a58d
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-13 22:07:40 -0400

    fix/had two widths

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -4,12 +4,12 @@ function Admin() {
 
     return (
         <div className="grid grid-cols-1 gap-4 place-items-center">
-            <div className="bg-slate-500 text-white w-full rounded p-2 mt-8 w-fit text-center shadow-2xl">
+            <div className="bg-slate-500 text-white rounded p-2 mt-8 w-fit text-center shadow-2xl">
                 <Link to='/admin/projects' className='p-4'>Projects</Link>
             </div>
 
-            <div className="bg-slate-500 text-white w-full rounded p-2 mt-8 w-fit text-center shadow-2xl">
-                <Link to='/admin/projects/create' className='p-4'>Create project</Link>
+            <div className="bg-slate-500 text-white rounded p-2 mt-8 w-fit text-center shadow-2xl">
+                <Link to='/admin/projects/create' className='p-4'>reate project</Link>
             </div>
 
         </div>
