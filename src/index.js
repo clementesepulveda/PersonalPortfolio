e721c8b0f6ac8db77c61c87dfdf439348cc5c0e1
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-15 16:21:07 -0400

    feat/vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router';
 
+import { inject } from '@vercel/analytics';
+
+inject();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
