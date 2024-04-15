fb7f64e8a2410bf8ab10185034a9d8083703a601
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-15 16:14:49 -0400

    feat/vercel analytics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+import { Analytics } from "@vercel/analytics/react"
+
 function Home() {
 
   return (
     <div className="App">
+      <Analytics />
+      
       <u>
         <ul><a href="/boids">Boids</a></ul>
         <ul><a href="/abstract">Fun stuffs</a></ul>
