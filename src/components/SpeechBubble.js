940c2e99d96e4524f63c93982a16ae1d1b257b8b
clementesepulveda <clemente.sepulveda@uc.cl>
2024-04-18 00:01:01 -0400

    smaller bubble

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -3,7 +3,7 @@ import './SpeechBubble.scss'
 
 export default function SpeechBubble({ text }) {
     return (
-        <blockquote className="speech-bubble">
+        <blockquote className="speech-bubble px-2 py-1 rounded-md">
             <p>{ text }</p>
         </blockquote>
     )
