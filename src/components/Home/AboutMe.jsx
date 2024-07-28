becb4c3150cbfd21caea5d3d199d24741a35cb56
clementesepulveda <clemente.sepulveda@uc.cl>
2024-07-28 12:32:55 -0400

    removed picture

--- src/components/Home/AboutMe.jsx
+++ src/components/Home/AboutMe.jsx
@@ -4,11 +4,11 @@ export default function AboutMe() {
     return (
         <>
             <div className='m-w-full text-justify grid sm:grid-cols-2 place-items-start'>
-                <img
+                {/* <img
                     src="https://i.ibb.co/yQVxVfx/360-F-57687532-x-RY2y9-BNw-KS7-USH36np-KWMx-Yj-Om5-Ov-OC-removebg-preview.png"
                     alt="me chilling"
                     className='sm:hidden size-fit scale-x-[-1]'
-                />
+                /> */}
 
                 <div className='p-2 sm:p-4 bg-slate-100 dark:bg-slate-700 rounded shadow-xl h-full'>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -18,11 +18,11 @@ export default function AboutMe() {
                     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
-                <img
+                {/* <img
                     src="https://i.ibb.co/yQVxVfx/360-F-57687532-x-RY2y9-BNw-KS7-USH36np-KWMx-Yj-Om5-Ov-OC-removebg-preview.png"
                     alt="me chilling"
                     className='hidden sm:block size-fit scale-x-[-1]'
-                />
+                /> */}
             </div>
 
             <style>{`
