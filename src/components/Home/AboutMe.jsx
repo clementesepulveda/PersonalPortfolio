f5bedfa4b541a6bbe6524e4d7dab2c51b5e73eeb
clementesepulveda <clemente.sepulveda@uc.cl>
2024-10-09 20:25:34 -0300

    fix: captivating paragraph

--- src/components/Home/AboutMe.jsx
+++ src/components/Home/AboutMe.jsx
@@ -11,12 +11,10 @@ export default function AboutMe() {
                 /> */}
 
                 <div className='p-2 sm:p-4 bg-slate-100 dark:bg-slate-700 rounded shadow-xl h-full'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Welcome to my digital space! I'm Clemente, a passionate developer with a versatile skill set ranging from full-stack engineering to game development and data science. 
+                My work is a blend of creative problem-solving and technical expertise, all driven by a desire to bring innovative ideas to life. 
+                Whether I'm designing intuitive user interfaces or building powerful data-driven solutions, I thrive on transforming challenges into polished, functional outcomes. 
+                Take a look around, and let's create something remarkable together.
                 </div>
                 {/* <img
                     src="https://i.ibb.co/yQVxVfx/360-F-57687532-x-RY2y9-BNw-KS7-USH36np-KWMx-Yj-Om5-Ov-OC-removebg-preview.png"
